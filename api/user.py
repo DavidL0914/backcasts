2663a3bdded008fafdb9c9e14cdae2c4c0ba72e8
7mwang <matthewxpwang@gmail.com>
2024-02-15 10:24:07 -0800

    cookie option change

--- api/user.py
+++ api/user.py
@@ -129,7 +129,7 @@ class UserAPI:
                             algorithm="HS256"
                         )
                         resp = Response("Authentication for %s successful" % (user._uid))
-                        resp.set_cookie(key="jwt", value=token, max_age=3600, secure=True, samesite="Lax", path='/', httponly=True)
+                        resp.set_cookie(key="jwt", value=token, max_age=3600, secure=True, samesite='None', path='/', httponly=False)
                         print(resp.headers) 
                         return resp
                     except Exception as e:
